25f19d29764f9661b88d8030ef0461af36b03b35
Jason Bryer <jason@bryer.org>
2014-04-22 12:42:18 -0400

    Added installation of gitbook-pdf. Changed the error message if the installation fails.

--- R/installGitbook.R
+++ R/installGitbook.R
@@ -4,11 +4,26 @@
 #' regardless if the current version. That is, this function will either update
 #' or install a new version of Gitbook.io.
 #' 
+#' On many systems this function will not work as installing requires root
+#' access. The function will print the required commands for you to copy-and-paste
+#' to the terminal.
+#' 
 #' @export
 installGitbook <- function() {
-	test <- system('sudo -A npm install gitbook -g')
-	if(test != 0) { stop(paste0("gitbook installation failed. Typically ", 
-								"installing as root/Administrator works:\n",
-								"  sudo npm install gitbook -g")) }
+	cmds <- c('npm install gitbook -g',
+			  'npm install gitbook-pdf -g')
+	for(i in cmds) {
+		if(Sys.info()['sysname'] == 'Windows') {
+			cmd <- i
+		} else {
+			cmd <- paste0('sudo -A ', i)
+			#test <- system(cmd, input=readline("Enter your password: "))
+		}
+		test <- system(cmd)
+		if(test != 0) { stop(paste0("gitbook installation failed. Typically ", 
+									"installing as root/Administrator ",
+									"from the terminal/console works:\n",
+									paste0('   ', cmds , collapse='\n'))) }		
+	}
 	invisible()
 }
